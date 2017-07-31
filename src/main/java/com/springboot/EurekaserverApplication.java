b4ad0a10560fadae3d75221aa4ef1113c33230f0
yangqing <736875071@qq.com>
2017-07-31 11:38:25 +0800

    eureka-service项目初始化

---
+++ src/main/java/com/springboot/EurekaserverApplication.java
@@ -0,0 +1,15 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer         //开启eureka服务
+@SpringBootApplication
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+}
